39824bea2d3f97bf07715496a947328d44e664a4
hillary nicholas <75504607+hill-nicholas@users.noreply.github.com>
2023-12-11 14:38:53 +0100

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+export const Data = [
+    {
+        id:1,
+        mainImage:"./images/image-product-1.jpg",
+        thumbnail:"./images/image-product-1-thumbnail.jpg",
+        productName:"Fall Limited Edition",
+        price: 250,
+    },
+    {
+        id:2,
+        mainImage:"./images/image-product-2.jpg",
+        thumbnail:"./images/image-product-2-thumbnail.jpg",
+        productName:"Risen Limited Edition",
+        price: 230,
+    },
+    {
+        id:3,
+        mainImage:"./images/image-product-3.jpg",
+        thumbnail:"./images/image-product-3-thumbnail.jpg",
+        productName:"Deluxe Limited Edition",
+        price: 320,
+    },
+    {
+        id:4,
+        mainImage:"./images/image-product-4.jpg",
+        thumbnail:"./images/image-product-4-thumbnail.jpg",
+        productName:"Exclusive Custom Edition",
+        price: 180,
+    }
+]
